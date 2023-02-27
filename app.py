2058bb70bd88e6bb918e7873c358a45615fad4be
alexzanotti <alexanderzanotti@hotmail.com>
2023-02-27 09:49:01 +0000

    applicant routes

--- app.py
+++ app.py
@@ -2,3 +2,18 @@
 @app.route("/")
 def index():
     return render_template("index.html", title="Home", header="Home")
+
+# Applicants Applied Page
+@app.route("/applicants_applied")
+def applicants_applied():
+    return render_template("applicants_applied.html", title="Applicants Applied", header="Applicants Applied")
+
+# Applicants Apply Page
+@app.route("/applicants_apply")
+def applicants_apply():
+    return render_template("applicants_apply.html", title="Apply", header="Apply")
+
+# Applicants Page
+@app.route("/applicants")
+def applicants():
+    return render_template("applicants.html", title="Applicants", header="Applicants")
