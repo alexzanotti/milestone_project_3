ab0474c368a8c91e0593592ebb79ce42348c2925
alexzanotti <alexanderzanotti@hotmail.com>
2023-03-05 12:07:21 +0000

    applicant data model

--- data_models.py
+++ data_models.py
@@ -16,3 +16,18 @@ class Listing(db.Model):
         # __repr__ to represent itself in the form of a string
         return self.job_title
       
+class Applicant(db.Model):
+    # schema for the Applicant model
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(50), nullable=False)
+    experience_level = db.Column(db.String(50), nullable=False)
+    personal_statement = db.Column(db.Text, nullable=False)
+    education = db.Column(db.Text, nullable=False)
+    work_experience = db.Column(db.Text, nullable=False)
+    listing_id = db.Column(db.Integer, db.ForeignKey("listing.id", ondelete="CASCADE"), nullable=False)
+
+    def __repr__(self):
+        # __repr__ to represent itself in the form of a string
+        return self.name
